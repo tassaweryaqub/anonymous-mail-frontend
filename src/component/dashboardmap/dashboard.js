aaf3a51e9201465945d0a83059eb0c4d0f443475
= <=>
2020-05-07 20:01:39 +0200

    updated code

--- src/component/dashboardmap/dashboard.js
+++ src/component/dashboardmap/dashboard.js
@@ -1,73 +1,153 @@
-import React from "react";
-
-// Material-UI
-import { makeStyles } from "@material-ui/core/styles";
-
-
-// Image
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeSharpIcon from '@material-ui/icons/HomeSharp';
+import BlockSharpIcon from '@material-ui/icons/BlockSharp';
+import ReplySharpIcon from '@material-ui/icons/ReplySharp';
+import PriorityHighSharpIcon from '@material-ui/icons/PriorityHighSharp';
+import SettingsSharpIcon from '@material-ui/icons/SettingsSharp';
+import ExitToAppSharpIcon from '@material-ui/icons/ExitToAppSharp';
 import logo from "../../img/Icon.png";
-import dbbord from "../../img/Rectangle 14.png"; 
-import striped from "../../img/Drawer btn.png"; 
-import homedash from "../../img/Home.png";
-import line1 from "../../img/Line 10.png"; 
-import block from "../../img/block-helper.png"; 
-import secline from "../../img/Line 11.png"; 
-import reply from "../../img/Reply.png"; 
-import thirdline from "../../img/Line 12.png"; 
-import trash from "../../img/spam 1.png"; 
-import line4 from "../../img/Line 14.png"; 
-import settings from "../../img/Settings icon.png"; 
-import line5 from "../../img/Line 13.png"; 
-import logout from "../../img/Logout icon.png"; 
 import plus from "../../img/plus.png";
-// CSS
-import "../../main.css";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    flexGrow: 1
-  }
-}));
 
-export default function ButtonAppBar() {
-  const klassen = useStyles();
+{/*stijl element */}
+
+const useStyles = makeStyles({
+  list: {
+    background: '#2980B9',
+    background: '-webkit-linear-gradient(to top, #FFFFFF, #6DD5FA, #2980B9)',  
+    background: 'linear-gradient(to top, #FFFFFF, #6DD5FA, #2980B9)',
+    borderRadius: 2,
+    width: 175, 
+    height:1900,
+    padding: 100,
+   
+  },
+  fullList: {
+    width: 'auto', 
+    
+  },
+});
+
+export default function SwipeableTemporaryDrawer() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    left: false
+   
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+              {/* Dashboard Navigatie */}
+      <List>
+        {['Home'].map((text) => (
+          <ListItem button key={text}>
+            <ListItemIcon> <HomeSharpIcon /></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['Blocker Users'].map((text) => (
+          <ListItem button key={text}>
+            <ListItemIcon> <BlockSharpIcon /></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['Auto Replies'].map((text) => (
+          <ListItem button key={text}>
+            <ListItemIcon> <ReplySharpIcon /></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['Spammed Users'].map((text) => (
+          <ListItem button key={text}>
+            <ListItemIcon> <PriorityHighSharpIcon /></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['Settings'].map((text) => (
+          <ListItem button key={text}>
+            <ListItemIcon> <SettingsSharpIcon /></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['Signout'].map((text) => (
+          <ListItem button key={text}>
+            <ListItemIcon> <ExitToAppSharpIcon /></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </div>
+  );
 
   return (
-    <div className={klassen.root}>
-        {/* Dashboard sectie*/ }
-      <a href= "/"><img className= "db-logo" src={logo}  width="70" height="50" alt="logo" /></a>  
-        <img className= "db-bord" src={dbbord} />  
-            <img className= "stripedbtn" src={striped} /> {/* gestreepte button dashboard */}
-            <a href= "#"><img className= "homedash" src={homedash} /></a> {/* home van dashboard */}
-              <img className= "eerstelijn" src={line1} /> 
-              <a href= "#"><img className= "block" src= {block} /></a>  {/* block logo dashboard */}
-              <img className= "tweedelijn" src={secline} /> 
-              <a href= "#"><img className= "reply" src={reply} /></a>  {/* antwoord logo*/ }
-              <img className= "derdelijn" src={thirdline} /> 
-              <a href= "#"><img className= "trash" src={trash} /></a>  {/*trash logo van dashboard */}
-              <img className= "vierdelijn" src={line4} /> 
-              <a href= "#"><img className= "settings" src={settings} /></a>  {/*settings logo van dashboard */}
-              <img className= "vijfdelijn" src={line5} /> 
-              <a href= "#"><img className= "logoutlogo" src={logout} /></a>  {/* logout logo van dashboard */}
-              <a href= "#"><img className= "plusicon" src={plus} /></a>  {/* plus icoon van dashboard */}
+    <div>
+      {['OPEN MENU'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button className= "knopje" onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+             {/* logo image */}
+             <a href= "/dashboard"><img className= "db-logo" src={logo} width="70" height="50" alt="logo" /></a> 
 
-            <p className= "get5">Get 5 latest Email Subjects</p>
+              {/* voorlopige tekst getallen moeten nog verwerkt worden in de backend */}
+              <p className= "inbox">Inbox: 250 emails </p>
+              <p className= "trashtext">Trash: 25 mails </p>
+              <p className= "readtext">Read: 5 emails </p>
+              <p className= "unreaden">Unreaden: 245 emails </p>
+              <p className= "labeltext">Labels: 5 </p>
 
-            {/*aantallen voor inkomende emails, is tijdelijk totdat backend geïmplementeerd is*/}
-           <p className= "inbox">Inbox: 250 emails </p>
-           <p className= "trashtext">Trash: 25 mails </p>
-           <p className= "readtext">Read: 5 emails </p>
-           <p className= "unreaden">Unreaden: 245 emails </p>
-           <p className= "labeltext">Labels: 5 </p>
-
-      <h2 className="vl-dashboard">Overview</h2>
-
-      </div>
+              <a href= "/dashboard"><img className= "plusicon" src={plus} /></a>  {/* plus icoon van dashboard */}
+       
+    </div>
   );
 }
+
+
